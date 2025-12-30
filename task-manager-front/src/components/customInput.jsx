3f6f166bf3db4928b1726d5ec0416672b193d927
tiago19deoliveira <tiago18deoliveira@gmail.com>
2025-12-29 22:05:33 -0400

    fix: custom input label

--- task-manager-front/src/components/customInput.jsx
+++ task-manager-front/src/components/customInput.jsx
@@ -1,11 +1,19 @@
-const CustomInput = ({ label, value }) => {
+import "./customInput.scss";
+
+const CustomInput = ({ label, value, onChange }) => {
   return (
     <div className="custom-input-container">
-      <input type="text" className="custom-input" />
+      <input
+        type="text"
+        className="custom-input"
+        onChange={(e) => onChange(e)}
+      />
       {label ? (
         <label
           className={`${value.length > 0 ? "shrink" : ""} custom-input-label`}
-        ></label>
+        >
+          {label}
+        </label>
       ) : null}
     </div>
   );
