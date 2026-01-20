dcb474ea24719aafe7a9bb7325b6a768acaf8641
tiago19deoliveira <tiago18deoliveira@gmail.com>
2026-01-19 22:24:36 -0400

    fix: add return and icon to custombutton

--- task-manager-front/src/components/addTask.jsx
+++ task-manager-front/src/components/addTask.jsx
@@ -1,6 +1,8 @@
 import CustomInput from "./customInput";
+import CustomButton from "./customButton";
 import "./addTask.scss";
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
@@ -11,6 +13,9 @@ const AddTask = () => {
   return (
     <div className="add-task-container">
       <CustomInput label="Adicionar..." value={task} onChange={onChange} />
+      <CustomButton>
+        <FaPlus size={14} color="#ffff" />
+      </CustomButton>
     </div>
   );
 };
